fb87f9f86df251ca30a9e03b1fd6cc4ee7568ddc
Jochen Peuker <jochen.peuker@binaris-informatik.de>
2021-02-10 19:50:37 +0100

    add statics tests

--- src/test/java/de/PMD_Report_Extractor/AppTest.java
+++ src/test/java/de/PMD_Report_Extractor/AppTest.java
@@ -2,18 +2,22 @@ package de.PMD_Report_Extractor;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockito.ArgumentMatchers.any;
 
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Set;
 
 import javax.xml.parsers.ParserConfigurationException;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
 import org.xml.sax.SAXException;
 
 import de.PMD_Report_Extractor.util.ReportFormatter;
@@ -22,18 +26,6 @@ import de.PMD_Report_Extractor.util.ResourceFileHandler;
 
 @ExtendWith(MockitoExtension.class)
 public class AppTest {
-	@InjectMocks
-	private App app;
-
-	@Mock
-	private ResourceFileHandler fileHandler;
-
-	@Mock
-	private ReportParser parser;
-
-	@Mock
-	private ReportFormatter formatter;
-
 	@Test
 	void main_no_argsTest() {
 		String[] args = {};
@@ -48,9 +40,25 @@ public class AppTest {
 	@Test
 	void main_argsTest() throws IOException, ParserConfigurationException, SAXException {
 		String[] args = { "reportPath", "excludePath", "rule" };
+		Document xml = Mockito.mock(Document.class);
+		Set<Node> filteredNodes = Collections.emptySet();
+		StringBuffer sbExtract = new StringBuffer().append("extract");
+		StringBuffer sbExclude = new StringBuffer().append("exclude");
+		StringBuffer sbMerge = new StringBuffer().append("merge");
 
-		fail("TODO - refactor classes");
+		try (MockedStatic<ResourceFileHandler> fileHandlerMock = Mockito.mockStatic(ResourceFileHandler.class);
+				MockedStatic<ReportParser> parserMock = Mockito.mockStatic(ReportParser.class);
+				MockedStatic<ReportFormatter> fomatterMock = Mockito.mockStatic(ReportFormatter.class)) {
+			fileHandlerMock.when(() -> ResourceFileHandler.readFile(args[0])).thenReturn(new StringBuffer());
+			parserMock.when(() -> ReportParser.parseToXml(any(StringBuffer.class))).thenReturn(xml);
+			parserMock.when(() -> ReportParser.filterByRule(xml, args[2])).thenReturn(filteredNodes);
+			fomatterMock.when(() -> ReportFormatter.formatToExcludeStyle(filteredNodes)).thenReturn(sbExtract);
+			fileHandlerMock.when(() -> ResourceFileHandler.readFile(args[1])).thenReturn(sbExclude);
+			fomatterMock.when(() -> ReportFormatter.merge(sbExclude, sbExtract)).thenReturn(sbMerge);
 
-		App.main(args);
+			App.main(args);
+
+			fileHandlerMock.verify(() -> ResourceFileHandler.writeToFile(args[1], sbMerge));
+		}
 	}
 }
