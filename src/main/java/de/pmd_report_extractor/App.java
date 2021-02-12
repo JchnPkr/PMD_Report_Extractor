6fae16b7def2655cfd0b83d33a9e1cfe4b10e6cb
Jochen Peuker <jochen.peuker@binaris-informatik.de>
2021-02-12 03:05:03 +0100

    add error handling

--- src/main/java/de/pmd_report_extractor/App.java
+++ src/main/java/de/pmd_report_extractor/App.java
@@ -22,7 +22,7 @@ public class App {
 	/**
 	 * Expects three arguments separated with a blank space. 1. the path to the
 	 * report file, 2. the path to the exclude file, 3. the rule to add to the
-	 * exclude file
+	 * exclude file.
 	 * 
 	 * @param args
 	 *                 takes the arguments from console
@@ -33,7 +33,7 @@ public class App {
 	 * @throws SAXException
 	 *                                          Exception
 	 */
-	public static void main(String[] args) throws IOException, ParserConfigurationException, SAXException {
+	public static void main(String[] args) {
 		LOG.info("rule extraction started...");
 
 		if (args.length == 3) {
@@ -41,22 +41,50 @@ public class App {
 			final String pathToExcludeResource = args[1];
 			final String rule = args[2];
 
-			StringBuilder sbIn = ResourceFileHandler.readFile(pathToPmdResource);
-			Document xml = ReportParser.parseToXml(sbIn);
-			Set<Node> filteredNodes = ReportParser.filterByRule(xml, rule);
-			StringBuilder sbExtract = ReportFormatter.formatToExcludeStyle(filteredNodes);
-			ResourceFileHandler.writeToConsole(sbExtract, "result from extraction");
-
-			StringBuilder sbExclude = ResourceFileHandler.readFile(pathToExcludeResource);
-			StringBuilder sbMerge = ReportFormatter.merge(sbExclude, sbExtract);
-			ResourceFileHandler.writeToConsole(sbMerge, "result from merge");
-			ResourceFileHandler.writeToFile(pathToExcludeResource, sbMerge);
+			try {
+				process(pathToPmdResource, pathToExcludeResource, rule);
+				LOG.info("report extracted successfully");
+			} catch (IOException e) {
+				LOG.error("Error reading / writing file: {}", e.getMessage());
+			} catch (ParserConfigurationException | SAXException e) {
+				LOG.error("Error parsing XML: {}", e.getMessage());
+			}
 		} else {
 			throw new IllegalArgumentException(
 					"Wrong number of arguments. A path to a PMD xml report, a path to an exlude file "
 							+ "and a PMD rule are mandatory.\n Arguments given: " + Arrays.toString(args));
 		}
 
-		LOG.info("report extracted successfully");
+		LOG.info("...shutting down");
+	}
+
+	/**
+	 * The real work get's done here.
+	 * 
+	 * @param pathToPmdResource
+	 *                                  path to report file
+	 * @param pathToExcludeResource
+	 *                                  path to exclude file
+	 * @param rule
+	 *                                  rule to exclude
+	 * @throws IOException
+	 *                                          Exception
+	 * @throws ParserConfigurationException
+	 *                                          Exception
+	 * @throws SAXException
+	 *                                          Exception
+	 */
+	private static void process(final String pathToPmdResource, final String pathToExcludeResource, final String rule)
+			throws IOException, ParserConfigurationException, SAXException {
+		StringBuilder sbIn = ResourceFileHandler.readFile(pathToPmdResource);
+		Document xml = ReportParser.parseToXml(sbIn);
+		Set<Node> filteredNodes = ReportParser.filterByRule(xml, rule);
+		StringBuilder sbExtract = ReportFormatter.formatToExcludeStyle(filteredNodes);
+		ResourceFileHandler.writeToConsole(sbExtract, "result from extraction");
+
+		StringBuilder sbExclude = ResourceFileHandler.readFile(pathToExcludeResource);
+		StringBuilder sbMerge = ReportFormatter.merge(sbExclude, sbExtract);
+		ResourceFileHandler.writeToConsole(sbMerge, "result from merge");
+		ResourceFileHandler.writeToFile(pathToExcludeResource, sbMerge);
 	}
 }
--- src/test/java/de/pmd_report_extractor/AppTest.java
+++ src/test/java/de/pmd_report_extractor/AppTest.java
@@ -1,18 +1,34 @@
 package de.pmd_report_extractor;
 
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Set;
+import java.util.stream.Stream;
 
 import javax.xml.parsers.ParserConfigurationException;
 
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.core.Appender;
+import org.apache.logging.log4j.core.LogEvent;
+import org.apache.logging.log4j.core.Logger;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.Mock;
 import org.mockito.MockedStatic;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -20,13 +36,30 @@ import org.w3c.dom.Document;
 import org.w3c.dom.Node;
 import org.xml.sax.SAXException;
 
-import de.pmd_report_extractor.App;
 import de.pmd_report_extractor.util.ReportFormatter;
 import de.pmd_report_extractor.util.ReportParser;
 import de.pmd_report_extractor.util.ResourceFileHandler;
 
 @ExtendWith(MockitoExtension.class)
 public class AppTest {
+	private static final String ERRMSG = "test message";
+
+	@Mock
+	private Appender mockedAppender;
+
+	@Captor
+	private ArgumentCaptor<LogEvent> loggingEventCaptor;
+
+	@BeforeEach
+	void setup() {
+		when(mockedAppender.getName()).thenReturn("mockedAppender");
+		when(mockedAppender.isStarted()).thenReturn(true);
+
+		Logger root = (Logger) LogManager.getRootLogger();
+		root.addAppender(mockedAppender);
+		root.setLevel(Level.ERROR);
+	}
+
 	@Test
 	void main_no_argsTest() {
 		String[] args = {};
@@ -63,5 +96,50 @@ public class AppTest {
 			fileHandlerMock.verify(() -> ResourceFileHandler.writeToConsole(sbExtract, "result from extraction"));
 			fileHandlerMock.verify(() -> ResourceFileHandler.writeToConsole(sbMerge, "result from merge"));
 		}
+	}
+
+	@Test
+	void main_IOExceptionTest() {
+		String expectedMsg1 = "Error reading / writing file: " + ERRMSG;
+
+		try (MockedStatic<ResourceFileHandler> fileHandlerMock = Mockito.mockStatic(ResourceFileHandler.class)) {
+			fileHandlerMock.when(() -> ResourceFileHandler.readFile(any())).thenThrow(new IOException(ERRMSG));
+
+			App.main(new String[] { "", "", "" });
+
+			verify(mockedAppender, times(3)).append(loggingEventCaptor.capture());
+
+			LogEvent logEvent2 = loggingEventCaptor.getAllValues().get(1);
+
+			assertAll(
+					() -> assertEquals(expectedMsg1, logEvent2.getMessage().getFormattedMessage()),
+					() -> assertEquals(Level.ERROR, logEvent2.getLevel()));
+		}
+	}
+
+	@ParameterizedTest
+	@MethodSource("exceptionProvider")
+	void main_XMLExceptionTest(Exception e) {
+		String expectedMsg1 = "Error parsing XML: " + ERRMSG;
+
+		try (MockedStatic<ResourceFileHandler> fileHandlerMock = Mockito.mockStatic(ResourceFileHandler.class);
+				MockedStatic<ReportParser> parserMock = Mockito.mockStatic(ReportParser.class)) {
+			fileHandlerMock.when(() -> ResourceFileHandler.readFile(any())).thenReturn(new StringBuilder());
+			parserMock.when(() -> ReportParser.parseToXml(any(StringBuilder.class))).thenThrow(e);
+
+			App.main(new String[] { "", "", "" });
+
+			verify(mockedAppender, times(3)).append(loggingEventCaptor.capture());
+
+			LogEvent logEvent2 = loggingEventCaptor.getAllValues().get(1);
+
+			assertAll(
+					() -> assertEquals(expectedMsg1, logEvent2.getMessage().getFormattedMessage()),
+					() -> assertEquals(Level.ERROR, logEvent2.getLevel()));
+		}
+	}
+
+	private static Stream<Exception> exceptionProvider() {
+		return Stream.of(new ParserConfigurationException(ERRMSG), new SAXException(ERRMSG));
 	}
 }
